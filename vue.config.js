1636ae546b877ffb14f685b6e2b2fb9795143483
Pavel Bakin <pavel@bakin.pro>
2019-08-11 01:24:13 +0700

    update

--- vue.config.js
+++ vue.config.js
@@ -1,25 +1,6 @@
-var path = require('path')
-var webpack = require('webpack');
-
-isDev = process.env.NODE_ENV === 'development'
-
+let isDev = process.env.NODE_ENV === 'development'
 
 module.exports = {
   publicPath: isDev ? '' : 'janus-vue-demo',
   outputDir: 'dist',
-  css: {
-    sourceMap: isDev ? true : false
-  },
-  configureWebpack: {
-    resolve: {
-      alias: {
-        '@src': path.resolve(__dirname, './src')
-      }
-    }
-  },
-  devServer: {
-    public: 'localhost:8080',
-    // sockHost: 'localhost:8080',
-    disableHostCheck: false
-  }
 }
